1d6fd5a5396530748163810ef9f9e61a48564b07
Luiz Felipe <ganassini.luiz@escola.pr.gov.br>
2025-09-09 08:46:27 -0300

    09/09

---
+++ Perguntas.js
@@ -0,0 +1,125 @@
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'Programação',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi o Marechal Deodoro da Fonseca, que assumiu o cargo em 15 de novembro de 1889, após a Proclamação da República. Eleito por voto indireto em 25 de fevereiro de 1891, tornando-se o primeiro presidente eleito do país. '
+)
+
+criaCartao(
+    'categoria',
+    'Qual é o país mais populoso do mundo?',
+    'O país mais populoso do mundo é a Índia.'
+)
+
+criaCartao(
+    'categoria',
+    'Quem é conhecido como o "Pai da Física"?',
+    'O título de "pai da física" é frequentemente atribuído a Isaac Newton.'
+)
+
+criaCartao(
+    'categoria',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
+
+criaCartao(
+    'categoria',
+    'De quem é a frase "Só sei que nada sei"?',
+    'A famosa frase "Só sei que nada sei" é atribuída ao filósofo grego Sócrates.'
+)
+
+criaCartao(
+    'categoria',
+    'Qual o único mamífero que voa?',
+    'O único mamífero que voa de verdade é o morcego. Outros mamíferos, como os esquilos voadores, apenas planam, não possuem a capacidade de voar ativamente.'
+)
+
+criaCartao(
+    'categoria',
+    'O que é a Páscoa?',
+    'A Páscoa é uma festa cristã que celebra a ressurreição de Jesus Cristo, ocorrida três dias após sua crucificação. É a celebração mais importante do cristianismo e marca o fim da Semana Santa, que é a última semana da Quaresma. Ovos e coelhos são símbolos comuns da Páscoa, com raízes em tradições pagãs que foram incorporadas ao cristianismo. '
+)
+
+criaCartao(
+    'categoria',
+    'Quantos ossos tem um corpo humano adulto?',
+    'O corpo humano adulto possui 206 ossos. No entanto, o número pode variar ligeiramente dependendo da fonte e de variações anatômicas individuais, como a presença de ossos acessórios.'
+)
+
+criaCartao(
+    'categoria',
+    'O que significa utopia?',
+    'lugar ou estado ideal, de completa felicidade e harmonia entre os indivíduos. Qualquer descrição imaginativa de uma sociedade ideal, fundamentada em leis justas e em instituições político-econômicas verdadeiramente comprometidas com o bem-estar da coletividade.'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
