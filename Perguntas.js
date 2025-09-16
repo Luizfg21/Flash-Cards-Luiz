11457d96a61c3b7949dfe1cc451327ce7cbcbabe
Luiz Felipe <ganassini.luiz@escola.pr.gov.br>
2025-09-16 09:46:06 -0300

    l

--- Perguntas.js
+++ Perguntas.js
@@ -1,125 +1,90 @@
+//1
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
-
-criaCartao(
-    'Programação',
+    'História',
     'Quem foi o primeiro presidente do Brasil?',
     'O primeiro presidente do Brasil foi o Marechal Deodoro da Fonseca, que assumiu o cargo em 15 de novembro de 1889, após a Proclamação da República. Eleito por voto indireto em 25 de fevereiro de 1891, tornando-se o primeiro presidente eleito do país. '
 )
-
+//2 
 criaCartao(
-    'categoria',
+    'Geografia',
     'Qual é o país mais populoso do mundo?',
     'O país mais populoso do mundo é a Índia.'
 )
-
+//3
 criaCartao(
-    'categoria',
+    'Física',
     'Quem é conhecido como o "Pai da Física"?',
     'O título de "pai da física" é frequentemente atribuído a Isaac Newton.'
 )
-
+//4
 criaCartao(
-    'categoria',
+    'Geografia',
     'Qual é o maior oceano do mundo?',
     'O maior oceano do mundo é o Oceano Pacífico.'
 )
-
+//5
 criaCartao(
-    'categoria',
+    'Filosofia',
     'De quem é a frase "Só sei que nada sei"?',
     'A famosa frase "Só sei que nada sei" é atribuída ao filósofo grego Sócrates.'
 )
-
+//6
 criaCartao(
-    'categoria',
+    'Biologia',
     'Qual o único mamífero que voa?',
     'O único mamífero que voa de verdade é o morcego. Outros mamíferos, como os esquilos voadores, apenas planam, não possuem a capacidade de voar ativamente.'
 )
-
+//7
 criaCartao(
-    'categoria',
+    'Teologia',
     'O que é a Páscoa?',
     'A Páscoa é uma festa cristã que celebra a ressurreição de Jesus Cristo, ocorrida três dias após sua crucificação. É a celebração mais importante do cristianismo e marca o fim da Semana Santa, que é a última semana da Quaresma. Ovos e coelhos são símbolos comuns da Páscoa, com raízes em tradições pagãs que foram incorporadas ao cristianismo. '
 )
-
+//8
 criaCartao(
-    'categoria',
+    'Biologia',
     'Quantos ossos tem um corpo humano adulto?',
     'O corpo humano adulto possui 206 ossos. No entanto, o número pode variar ligeiramente dependendo da fonte e de variações anatômicas individuais, como a presença de ossos acessórios.'
 )
-
+//9
 criaCartao(
-    'categoria',
+    'Português',
     'O que significa utopia?',
     'lugar ou estado ideal, de completa felicidade e harmonia entre os indivíduos. Qualquer descrição imaginativa de uma sociedade ideal, fundamentada em leis justas e em instituições político-econômicas verdadeiramente comprometidas com o bem-estar da coletividade.'
 )
-
+//10
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'História',
+    'Como são chamadas as estátuas da Ilha de Páscoa?',
+    'A Ilha de Páscoa inclui 900 estátuas monumentais chamadas moais.🗿'
 )
-
+//11
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Português',
+    'O é sofisma?',
+    'argumento ou raciocínio concebido com o objetivo de produzir a ilusão da verdade, que, embora simule um acordo com as regras da lógica, apresenta, na realidade, uma estrutura interna inconsistente, incorreta e deliberadamente enganosa. Pressupõe a não correspondência biunívoca entre o que o indivíduo pensa e o que ele diz.'
 )
-
+//12
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Grego',
+    'Quais são as primeiras quatro letras do alfabeto grego?',
+    'Α (alfa), Β (beta), Γ (gama) e Δ (delta)'
 )
-
+//12+1
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Inglês',
+    'O que significa a palavra (do inglês) “push”?',
+    'Empurrar.'
 )
-
+//14
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Português',
+    'Ao que se refere a palavra erudito?',
+    'A pessoa que tem vontade de aprender sempre, que está sempre estudando, procurando se aprofundar em assuntos cultos, sobre a história do mundo, das artes, da música, etc. Na Bíblia, o termo "erudito" refere-se a alguém com conhecimento profundo e vasto, adquirido principalmente através da leitura.'
 )
-
+//15
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
-
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
-
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
-
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
-
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
-)
-
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Grego',
+    'Qual é a última letra do alfabeto grego?',
+    'A última letra do alfabeto grego é Ômega (Ω, ω).'
 )
