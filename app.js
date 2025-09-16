cd6d69b2622f7e11e1b9bd137ef010ddb7001e28
Luiz Felipe <ganassini.luiz@escola.pr.gov.br>
2025-09-16 10:48:56 -0300

    16/09

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>`
 
+let respostaEstaVizivel = false
+
+function viraCartao(){
+    respostaEstaVizivel =! respostaEstaVizivel
+}
+
+
+
     container.appendChild(cartao)
 
 }
